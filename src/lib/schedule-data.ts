776fe7a0786c2b8a1bd6d7ce4cfa0ec5ab84074e
Amin Əlizadə <aminelizade2004.12.07@gmail.com>
2026-03-02 19:35:37 +0000

    { day: 'Bazar ertəsi', time: '14:05-15:25', subject: 'Obyektyönlü proqra

--- src/lib/schedule-data.ts
+++ src/lib/schedule-data.ts
@@ -2,337 +2,220 @@
 import { ClassSession } from "./types";
 
 export const FIXED_SCHEDULE: ClassSession[] = [
-  // --- AŞAĞI ALTQRUP ---
+  // --- BAZAR ERTƏSİ ---
   {
-    id: 'asagi-1-1',
+    id: 'it24.1-1-1-ust',
+    name: 'Obyektyönlü proqramlaşdırma (Məşğələ)',
+    teacher: 'Validə Nuriyeva',
+    day: 1,
+    startTime: '14:05',
+    endTime: '15:25',
+    subgroup: 'hamisi',
+    week: 'ust',
+    room: '311'
+  },
+  {
+    id: 'it24.1-1-1-alt',
+    name: 'Diskret riyaziyyat (Məşğələ)',
+    teacher: 'Lalə Rzayeva',
+    day: 1,
+    startTime: '14:05',
+    endTime: '15:25',
+    subgroup: 'hamisi',
+    week: 'alt',
+    room: '303'
+  },
+  {
+    id: 'it24.1-1-2',
+    name: 'Verilənlər bazası sistemləri (Mühazirə)',
+    teacher: 'Rəsmiyyə Əmiraslanova',
+    day: 1,
+    startTime: '15:35',
+    endTime: '16:55',
+    subgroup: 'hamisi',
+    week: 'hamisi',
+    room: '305'
+  },
+  {
+    id: 'it24.1-1-3-yuxari',
+    name: 'Kompüter şəbəkələri (Laboratoriya)',
+    teacher: 'Ceyhun Əlizadə',
+    day: 1,
+    startTime: '17:05',
+    endTime: '18:25',
+    subgroup: 'yuxari',
+    week: 'hamisi',
+    room: '308'
+  },
+  {
+    id: 'it24.1-1-3-asagi',
     name: 'Əməliyyat sistemləri (Laboratoriya)',
     teacher: 'Elnur Xəlilov',
     day: 1,
-    startTime: '12:30',
-    endTime: '13:50',
-    subgroup: 'asagi',
-    week: 'hamisi',
-    room: '309'
-  },
-  {
-    id: 'asagi-1-2',
-    name: 'Kompüter Şəbəkələri (Laboratoriya)',
-    teacher: 'Ceyhun Əlizadə',
-    day: 1,
-    startTime: '14:05',
-    endTime: '15:25',
-    subgroup: 'asagi',
-    week: 'hamisi',
-    room: '309'
-  },
-  {
-    id: 'asagi-1-3',
-    name: 'Verilənlər bazası sistemləri (Mühazirə)',
-    teacher: 'Rəsmiyyə Əmiraslanova',
-    day: 1,
-    startTime: '15:35',
-    endTime: '16:55',
-    subgroup: 'asagi',
-    week: 'hamisi',
-    room: '305'
-  },
-  {
-    id: 'asagi-2-1',
-    name: 'Diskret riyaziyyat (Mühazirə)',
-    teacher: 'Lalə Rzayeva',
-    day: 2,
-    startTime: '12:30',
-    endTime: '13:50',
-    subgroup: 'asagi',
-    week: 'hamisi',
-    room: '306'
-  },
-  {
-    id: 'asagi-2-2',
-    name: 'Kompüter Şəbəkələri (Mühazirə)',
-    teacher: 'Ceyhun Əlizadə',
-    day: 2,
-    startTime: '14:05',
-    endTime: '15:25',
-    subgroup: 'asagi',
-    week: 'hamisi',
-    room: '305'
-  },
-  {
-    id: 'asagi-2-3-ust',
-    name: 'Obyekt-yönümlü proqramlaşdırma (Laboratoriya)',
-    teacher: 'Məleykə Heydərova',
-    day: 2,
-    startTime: '15:35',
-    endTime: '16:55',
-    subgroup: 'asagi',
-    week: 'ust',
-    room: '313'
-  },
-  {
-    id: 'asagi-2-3-alt',
-    name: 'Verilənlər bazası sistemləri (Laboratoriya)',
-    teacher: 'Rəsmiyyə Əmiraslanova',
-    day: 2,
-    startTime: '15:35',
-    endTime: '16:55',
-    subgroup: 'asagi',
-    week: 'alt',
-    room: '313'
-  },
-  {
-    id: 'asagi-3-1',
-    name: 'Əməliyyat sistemləri (Mühazirə)',
-    teacher: 'Elnur Xəlilov',
-    day: 3,
-    startTime: '14:05',
-    endTime: '15:25',
-    subgroup: 'asagi',
-    week: 'hamisi',
-    room: '306'
-  },
-  {
-    id: 'asagi-3-2',
-    name: 'Obyekt-yönümlü proqramlaşdırma (Mühazirə)',
-    teacher: 'Validə Nuriyeva',
-    day: 3,
-    startTime: '15:35',
-    endTime: '16:55',
-    subgroup: 'asagi',
-    week: 'hamisi',
-    room: '306'
-  },
-  {
-    id: 'asagi-4-1-ust',
-    name: 'Kompüter Şəbəkələri (Mühazirə)',
-    teacher: 'Ceyhun Əlizadə',
-    day: 4,
-    startTime: '14:05',
-    endTime: '15:25',
-    subgroup: 'asagi',
-    week: 'ust',
-    room: '306'
-  },
-  {
-    id: 'asagi-4-2-ust',
-    name: 'Obyekt-yönümlü proqramlaşdırma (Məşğələ)',
-    teacher: 'Validə Nuriyeva',
-    day: 4,
-    startTime: '15:35',
-    endTime: '16:55',
-    subgroup: 'asagi',
-    week: 'ust',
-    room: '305'
-  },
-  {
-    id: 'asagi-4-3-ust',
-    name: 'Verilənlər bazası sistemləri (Məşğələ)',
-    teacher: 'Rəsmiyyə Əmiraslanova',
-    day: 4,
     startTime: '17:05',
     endTime: '18:25',
     subgroup: 'asagi',
-    week: 'ust',
-    room: '305'
-  },
-  {
-    id: 'asagi-4-1-alt',
-    name: 'Verilənlər bazası sistemləri (Mühazirə)',
-    teacher: 'Rəsmiyyə Əmiraslanova',
-    day: 4,
-    startTime: '14:05',
-    endTime: '15:25',
-    subgroup: 'asagi',
-    week: 'alt',
-    room: '306'
-  },
-  {
-    id: 'asagi-4-2-alt',
-    name: 'Diskret riyaziyyat (Məşğələ)',
-    teacher: 'Lalə Rzayeva',
-    day: 4,
-    startTime: '15:35',
-    endTime: '16:55',
-    subgroup: 'asagi',
-    week: 'alt',
-    room: '305'
-  },
-  {
-    id: 'asagi-4-3-alt',
-    name: 'Kompüter Şəbəkələri (Məşğələ)',
-    teacher: 'Ceyhun Əlizadə',
-    day: 4,
-    startTime: '17:05',
-    endTime: '18:25',
-    subgroup: 'asagi',
-    week: 'alt',
-    room: '305'
+    week: 'hamisi',
+    room: '309'
   },
 
-  // --- YUXARI ALTQRUP ---
+  // --- ÇƏRŞƏNBƏ AXŞAMI ---
   {
-    id: 'yuxari-1-1',
-    name: 'Kompüter Şəbəkələri (Laboratoriya)',
-    teacher: 'Ceyhun Əlizadə',
-    day: 1,
-    startTime: '12:30',
-    endTime: '13:50',
-    subgroup: 'yuxari',
-    week: 'hamisi',
-    room: '308'
-  },
-  {
-    id: 'yuxari-1-2',
-    name: 'Əməliyyat sistemləri (Laboratoriya)',
-    teacher: 'Elnur Xəlilov',
-    day: 1,
-    startTime: '14:05',
-    endTime: '15:25',
-    subgroup: 'yuxari',
-    week: 'hamisi',
-    room: '308'
-  },
-  {
-    id: 'yuxari-1-3',
-    name: 'Verilənlər bazası sistemləri (Mühazirə)',
-    teacher: 'Rəsmiyyə Əmiraslanova',
-    day: 1,
-    startTime: '15:35',
-    endTime: '16:55',
-    subgroup: 'yuxari',
-    week: 'hamisi',
-    room: '305'
-  },
-  {
-    id: 'yuxari-2-1',
+    id: 'it24.1-2-1',
     name: 'Diskret riyaziyyat (Mühazirə)',
     teacher: 'Lalə Rzayeva',
     day: 2,
     startTime: '12:30',
     endTime: '13:50',
-    subgroup: 'yuxari',
+    subgroup: 'hamisi',
     week: 'hamisi',
     room: '306'
   },
   {
-    id: 'yuxari-2-2',
-    name: 'Kompüter Şəbəkələri (Mühazirə)',
+    id: 'it24.1-2-2',
+    name: 'Kompüter şəbəkələri (Mühazirə)',
     teacher: 'Ceyhun Əlizadə',
     day: 2,
     startTime: '14:05',
     endTime: '15:25',
-    subgroup: 'yuxari',
+    subgroup: 'hamisi',
     week: 'hamisi',
     room: '305'
   },
   {
-    id: 'yuxari-2-3-ust',
-    name: 'Verilənlər bazası sistemləri (Laboratoriya)',
-    teacher: 'Rəsmiyyə Əmiraslanova',
+    id: 'it24.1-2-3-yuxari',
+    name: 'Əməliyyat sistemləri (Laboratoriya)',
+    teacher: 'Elnur Xəlilov',
     day: 2,
     startTime: '15:35',
     endTime: '16:55',
     subgroup: 'yuxari',
-    week: 'ust',
-    room: '311'
+    week: 'hamisi',
+    room: '308'
   },
   {
-    id: 'yuxari-2-3-alt',
-    name: 'Obyekt-yönümlü proqramlaşdırma (Laboratoriya)',
-    teacher: 'Validə Nuriyeva',
+    id: 'it24.1-2-3-asagi',
+    name: 'Kompüter şəbəkələri (Laboratoriya)',
+    teacher: 'Ceyhun Əlizadə',
     day: 2,
     startTime: '15:35',
     endTime: '16:55',
-    subgroup: 'yuxari',
-    week: 'alt',
-    room: '311'
+    subgroup: 'asagi',
+    week: 'hamisi',
+    room: '309'
   },
+
+  // --- ÇƏRŞƏNBƏ ---
   {
-    id: 'yuxari-3-1',
+    id: 'it24.1-3-1',
     name: 'Əməliyyat sistemləri (Mühazirə)',
     teacher: 'Elnur Xəlilov',
     day: 3,
     startTime: '14:05',
     endTime: '15:25',
-    subgroup: 'yuxari',
+    subgroup: 'hamisi',
     week: 'hamisi',
     room: '306'
   },
   {
-    id: 'yuxari-3-2',
-    name: 'Obyekt-yönümlü proqramlaşdırma (Mühazirə)',
+    id: 'it24.1-3-2',
+    name: 'Obyektyönlü proqramlaşdırma (Mühazirə)',
     teacher: 'Validə Nuriyeva',
     day: 3,
     startTime: '15:35',
     endTime: '16:55',
-    subgroup: 'yuxari',
+    subgroup: 'hamisi',
     week: 'hamisi',
     room: '306'
   },
   {
-    id: 'yuxari-4-1-ust',
-    name: 'Kompüter Şəbəkələri (Mühazirə)',
-    teacher: 'Ceyhun Əlizadə',
-    day: 4,
-    startTime: '14:05',
-    endTime: '15:25',
-    subgroup: 'yuxari',
-    week: 'ust',
-    room: '306'
-  },
-  {
-    id: 'yuxari-4-2-ust',
-    name: 'Obyekt-yönümlü proqramlaşdırma (Məşğələ)',
-    teacher: 'Validə Nuriyeva',
-    day: 4,
-    startTime: '15:35',
-    endTime: '16:55',
-    subgroup: 'yuxari',
-    week: 'ust',
-    room: '305'
-  },
-  {
-    id: 'yuxari-4-3-ust',
-    name: 'Verilənlər bazası sistemləri (Məşğələ)',
+    id: 'it24.1-3-3-yuxari-ust',
+    name: 'Verilənlər bazası sistemləri (Laboratoriya)',
     teacher: 'Rəsmiyyə Əmiraslanova',
-    day: 4,
+    day: 3,
     startTime: '17:05',
     endTime: '18:25',
     subgroup: 'yuxari',
     week: 'ust',
-    room: '305'
+    room: '308'
   },
   {
-    id: 'yuxari-4-1-alt',
+    id: 'it24.1-3-3-yuxari-alt',
+    name: 'Obyektyönlü proqramlaşdırma (Laboratoriya)',
+    teacher: 'Validə Nuriyeva',
+    day: 3,
+    startTime: '17:05',
+    endTime: '18:25',
+    subgroup: 'yuxari',
+    week: 'alt',
+    room: '309'
+  },
+  {
+    id: 'it24.1-3-3-asagi-ust',
+    name: 'Obyektyönlü proqramlaşdırma (Laboratoriya)',
+    teacher: 'Validə Nuriyeva',
+    day: 3,
+    startTime: '17:05',
+    endTime: '18:25',
+    subgroup: 'asagi',
+    week: 'ust',
+    room: '309'
+  },
+  {
+    id: 'it24.1-3-3-asagi-alt',
+    name: 'Verilənlər bazası sistemləri (Laboratoriya)',
+    teacher: 'Rəsmiyyə Əmiraslanova',
+    day: 3,
+    startTime: '17:05',
+    endTime: '18:25',
+    subgroup: 'asagi',
+    week: 'alt',
+    room: '308'
+  },
+
+  // --- CÜMƏ AXŞAMI ---
+  {
+    id: 'it24.1-4-1-ust',
+    name: 'Kompüter şəbəkələri (Mühazirə)',
+    teacher: 'Ceyhun Əlizadə',
+    day: 4,
+    startTime: '14:05',
+    endTime: '15:25',
+    subgroup: 'hamisi',
+    week: 'ust',
+    room: '306'
+  },
+  {
+    id: 'it24.1-4-1-alt',
     name: 'Verilənlər bazası sistemləri (Mühazirə)',
     teacher: 'Rəsmiyyə Əmiraslanova',
     day: 4,
     startTime: '14:05',
     endTime: '15:25',
-    subgroup: 'yuxari',
+    subgroup: 'hamisi',
     week: 'alt',
     room: '306'
   },
   {
-    id: 'yuxari-4-2-alt',
-    name: 'Diskret riyaziyyat (Məşğələ)',
-    teacher: 'Lalə Rzayeva',
+    id: 'it24.1-4-2-ust',
+    name: 'Verilənlər bazası sistemləri (Məşğələ)',
+    teacher: 'Rəsmiyyə Əmiraslanova',
     day: 4,
     startTime: '15:35',
     endTime: '16:55',
-    subgroup: 'yuxari',
-    week: 'alt',
-    room: '305'
+    subgroup: 'hamisi',
+    week: 'ust',
+    room: '306'
   },
   {
-    id: 'yuxari-4-3-alt',
-    name: 'Kompüter Şəbəkələri (Məşğələ)',
+    id: 'it24.1-4-2-alt',
+    name: 'Kompüter şəbəkələri (Məşğələ)',
     teacher: 'Ceyhun Əlizadə',
     day: 4,
-    startTime: '17:05',
-    endTime: '18:25',
-    subgroup: 'yuxari',
+    startTime: '15:35',
+    endTime: '16:55',
+    subgroup: 'hamisi',
     week: 'alt',
-    room: '305'
+    room: '306'
   }
 ];
