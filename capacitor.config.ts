e66a9417d736ed485157704e0c62b2edda2f5b41
Amin Elizade <aminelizade2004.12.07@gmail.com@gmail.com>
2026-03-03 01:20:56 +0400

    fix firebase client init

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.QrupTap.app',
+  appName: 'QrupTap',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  },
+};
+
+export default config;
---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+"use client";
+
+import { initializeApp, getApps } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDwXd_OkqSI1Y_nYYLvqt37cuwPBY1wtdU",
+  authDomain: "studio-5815475421-d853d.firebaseapp.com",
+  projectId: "studio-5815475421-d853d",
+  storageBucket: "studio-5815475421-d853d.firebasestorage.app",
+  messagingSenderId: "535350127210",
+  appId: "1:535350127210:web:e6f2f58cb45597f4767813"
+};
+
+export const app =
+  getApps().length === 0
+    ? initializeApp(firebaseConfig)
+    : getApps()[0];
